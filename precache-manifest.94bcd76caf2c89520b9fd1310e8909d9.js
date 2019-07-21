8f4cb2a4a380e86ccbdf2cd47f74cd0437dd2679
Ania Kujawa <kujawanna@gmail.com>
2019-07-21 21:50:51 +0200

    Updates

---
+++ precache-manifest.94bcd76caf2c89520b9fd1310e8909d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe5520bf718fe10ffe98fc3c19f5f8a",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "74d0fdfc411ccd783146",
+    "url": "/recipes-app/static/css/2.acad8a14.chunk.css"
+  },
+  {
+    "revision": "c3645c779fb57c830d87",
+    "url": "/recipes-app/static/css/main.0c45eeca.chunk.css"
+  },
+  {
+    "revision": "74d0fdfc411ccd783146",
+    "url": "/recipes-app/static/js/2.43a25aed.chunk.js"
+  },
+  {
+    "revision": "c3645c779fb57c830d87",
+    "url": "/recipes-app/static/js/main.2c4507c4.chunk.js"
+  },
+  {
+    "revision": "dfa723fb594534a9c68a",
+    "url": "/recipes-app/static/js/runtime~main.fdf092f1.js"
+  },
+  {
+    "revision": "97dfc10a035a7a6a0448daf6cae95d0e",
+    "url": "/recipes-app/static/media/background.97dfc10a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/recipes-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/recipes-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/recipes-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/recipes-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/recipes-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
